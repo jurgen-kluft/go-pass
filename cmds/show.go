3463f73e3eac236c243419aa8bbfb33bcaefd0b3
jurgen.kluft <jurgen.kluft@gmail.com>
2022-11-06 13:24:49 +0800

    * ShowCmd; can now output QR code

--- cmds/show.go
+++ cmds/show.go
@@ -5,15 +5,21 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/jurgen-kluft/go-pass/closestmatch"
+	"github.com/jurgen-kluft/go-pass/qrterminal"
 	"github.com/jurgen-kluft/go-pass/repo"
 )
 
 // - show [ --line[=line-number or field-name], -l[line-number or field-name] ] [--qrcode[=line-number or field-name], -q[line-number or field-name] ] pass-name
 
 type ShowCmd struct {
-	PassName string `arg:"" help:"Pass name."`
+	LineNumbers bool    `optional:"" short:"l" help:"Show the content with line numbers"`
+	Line        *int    `optional:"" short:"i" help:"Show the content of a specific line"`
+	Field       *string `optional:"" short:"f" help:"Show the content of a specific field"`
+	QrCode      *string `optional:"" short:"q" help:"Show the content of a specific field as a QR code (e.g. -q \"site\")"`
+	PassName    string  `arg:"" help:"Pass name."`
 }
 
 func (a *ShowCmd) Run(globals *Globals) error {
@@ -26,6 +32,7 @@ func (a *ShowCmd) Run(globals *Globals) error {
 
 	fmt.Println(r.Root)
 
+	qrcode := ""
 	if a.PassName != "" {
 		wordsToTest := []string{}
 		for _, gr := range r.Files {
@@ -48,7 +55,20 @@ func (a *ShowCmd) Run(globals *Globals) error {
 				fmt.Println()
 
 				err := repo.GetFileContentByLine(fullFilename, func(lineNumber int, line []byte) {
-					fmt.Printf("%d: %s\n", lineNumber, string(line))
+
+					if a.QrCode != nil {
+						if strings.HasPrefix(string(line), *a.QrCode) {
+							qrcode = string(line)
+						}
+					}
+
+					if a.LineNumbers {
+						fmt.Printf("%d: %s\n", lineNumber, string(line))
+					} else if a.Line != nil && *a.Line == lineNumber {
+						fmt.Printf("%s\n", string(line))
+					} else {
+						fmt.Printf("%s\n", string(line))
+					}
 				})
 				if err != nil {
 					fmt.Printf("Error: %s\n", err.Error())
@@ -56,5 +76,15 @@ func (a *ShowCmd) Run(globals *Globals) error {
 			}
 		}
 	}
+
+	if a.QrCode != nil && qrcode != "" {
+		// Generate a 'dense' qrcode with the 'Low' level error correction and write it to Stdout
+		qrcode = qrcode[len(*a.QrCode)+1:]
+		qrcode = strings.TrimSpace(qrcode)
+
+		fmt.Printf("QR code: %s\n", qrcode)
+		qrterminal.Generate(qrcode, qrterminal.L, os.Stdout)
+	}
+
 	return nil
 }
